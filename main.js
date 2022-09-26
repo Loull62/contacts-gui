1c8f488d1c7de9161a3b2416356075753b47439c
Lisa Jin <l.jin1@EDU.epsb.ca>
2022-09-26 14:49:09 -0600

    wa

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ let goBtnEl = document.getElementById('go-btn');
 let menuEl = document.getElementById('menu');
 let outputEl = document.getElementById('output');
 
+let contacts = loadContacts();
+displayContacts;
+
 // Go Btn - Menu Listener
 goBtnEl.addEventListener('click', goBtnHandler);
 
@@ -28,17 +31,33 @@ function goBtnHandler() {
 // MENU FUNCTIONS
 function displayContacts() {
   // console.log('Display Contacts');
-
+  let index = ''
+  for (i = 0; i > contacts.length; i++) {
+    outputStr += getTaskHTMLStr(tasks[i], i);
+  }
+  outputEl.innerHTML = outputStr;
 }
 
 function addContact() {
   console.log('Add Contact');
   let contactName = prompt("Give name")
   let contactEmail = prompt("Input Email")
+  let contactNum = prompt("Give number")
+  let contactCnty = prompt("Give country")
+  document.getElementById("output").innerHTML = "New Contact Added" + "(" + contactName + ")";
 }
 
 function removeContact() {
-  console.log('Remove Contact');
+  // console.log('Remove Contact');
+  let contactRmv = prompt("Contact number")
+  if (index >= 0 && index < tasks.length) {
+    //  Valid Index -> Remove
+    tasks.splice(index, 1);
+    saveContacts();
+    displayAll();
+  } else {
+    alert('Invalid Contact #')
+  }
 }
 
 function displayByName() {
@@ -48,3 +67,25 @@ function displayByName() {
 function displayByCountry() {
   console.log('Display by Country');
 }
+
+function saveContacts() {
+  localStorage.setItem('contacts', JSON.stringify(contacts))
+}
+
+function loadContacts() {
+  let contactsStr = localStorage.getItem('contacts');
+  return JSON.parse(contactsStr) ?? [];
+}
+
+function loadContacts() {
+}
+
+// let found = false;
+// for let()
+// if (so and so === thing)
+
+// if (found) {
+//   code
+// } else {
+//   things
+// }
